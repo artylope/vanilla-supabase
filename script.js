4ee9a34a703069a1c648f86ff53bcb43ff7eadd2
Ang Yi Xin <angyixin@gmail.com>
2023-06-14 17:56:40 +0800

    campaigns

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxYXNld3Z6dHhscHRvcm9yYWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU1MjgyNjAsImV4cCI6MjAwMTEwNDI2MH0.JJit0kmCJye8SUIrOZvQ482sgbqNh92uFDaa2gCPPKI';
 
 const { createClient } = supabase;
-const _supabase = createClient(supabaseUrl, supabaseKey);
+const database = createClient(supabaseUrl, supabaseKey);
 
-console.log('Supabase Instance: ', _supabase);
+console.log('Supabase Instance: ', database);
+
+const getCampaigns = async () => {
+  const res = await database.from('campaigns').select('*');
+  if (res) {
+    for (var i in res.data) {
+      console.log(res.data[i]);
+    }
+  }
+};
+
+getCampaigns();
